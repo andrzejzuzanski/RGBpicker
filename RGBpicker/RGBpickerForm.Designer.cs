1d5fa9515ca02e3d2eb3bbbd338cd1f3213ac4ef
Andrzej Zuzański <andrzej.zuzanski@bimobject.com>
2022-03-01 08:39:10 +0100

    Add project files.

---
+++ RGBpicker/RGBpickerForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace RGBpicker
+{
+    partial class RGBpickerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RGBpickerForm));
+            this.pickRGB = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gValue = new System.Windows.Forms.TextBox();
+            this.bValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.rValue = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hexValue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pickRGB
+            // 
+            this.pickRGB.Location = new System.Drawing.Point(8, 13);
+            this.pickRGB.Name = "pickRGB";
+            this.pickRGB.Size = new System.Drawing.Size(83, 23);
+            this.pickRGB.TabIndex = 0;
+            this.pickRGB.Text = "Pick RGB";
+            this.pickRGB.UseVisualStyleBackColor = true;
+            this.pickRGB.Click += new System.EventHandler(this.pickRGB_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(99, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // gValue
+            // 
+            this.gValue.Location = new System.Drawing.Point(37, 71);
+            this.gValue.Name = "gValue";
+            this.gValue.Size = new System.Drawing.Size(54, 23);
+            this.gValue.TabIndex = 3;
+            // 
+            // bValue
+            // 
+            this.bValue.Location = new System.Drawing.Point(37, 100);
+            this.bValue.Name = "bValue";
+            this.bValue.Size = new System.Drawing.Size(54, 23);
+            this.bValue.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(8, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(8, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(8, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(8, 129);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 48);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // rValue
+            // 
+            this.rValue.Location = new System.Drawing.Point(37, 42);
+            this.rValue.Name = "rValue";
+            this.rValue.Size = new System.Drawing.Size(54, 23);
+            this.rValue.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.hexValue);
+            this.groupBox1.Controls.Add(this.pickRGB);
+            this.groupBox1.Controls.Add(this.rValue);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.gValue);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.bValue);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(97, 248);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // hexValue
+            // 
+            this.hexValue.Location = new System.Drawing.Point(8, 186);
+            this.hexValue.Name = "hexValue";
+            this.hexValue.Size = new System.Drawing.Size(83, 23);
+            this.hexValue.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Copy #";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RGBpickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 387);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RGBpickerForm";
+            this.Text = "RGBpicker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pickRGB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox gValue;
+        private System.Windows.Forms.TextBox bValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox rValue;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox hexValue;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RGBpicker/RGBpickerForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+
+namespace RGBpicker
+{
+    public partial class RGBpickerForm : Form
+    {
+        public RGBpickerForm()
+        {
+            InitializeComponent();
+        }
+
+        public void pickRGB_Click(object sender, EventArgs e)
+        { 
+
+            this.Hide();
+
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                         Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            using (MemoryStream s = new MemoryStream())
+            {
+                printscreen.Save(s, ImageFormat.Bmp);
+                pictureBox1.Size = new System.Drawing.Size(this.Width, this.Height);
+
+                pictureBox1.Image = Image.FromStream(s);
+                s.Dispose();
+            }
+            this.Size = new Size(1200, 800);
+            this.Show();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            PropertyInfo imageRectangleProperty = typeof(PictureBox).GetProperty("ImageRectangle", BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (pictureBox1.Image != null)
+            {
+                MouseEventArgs me = (MouseEventArgs)e;
+
+                Bitmap original = (Bitmap)pictureBox1.Image;
+
+                Color? color = null;
+
+                color = original.GetPixel(me.X, me.Y);
+                string colorString = color.ToString();
+                string colorString1 = colorString.Replace("Color [A=255, ", "");
+                string colorString2 = colorString1.Replace("]", "");
+                string[] colorString3 = colorString2.Split(",");
+
+                rValue.Text = colorString3[0].Replace("R=", "");
+                gValue.Text = colorString3[1].Replace(" G=", "");
+                bValue.Text = colorString3[2].Replace(" B=", "");
+
+                int r = Convert.ToInt32(colorString3[0].Replace("R=", ""));
+                int g = Convert.ToInt32(colorString3[1].Replace(" G=", ""));
+                int b = Convert.ToInt32(colorString3[2].Replace(" B=", ""));
+
+                pictureBox2.BackColor = Color.FromArgb(r, g, b);
+
+                hexValue.Text = ColorTranslator.ToHtml(Color.FromArgb(r, g, b));
+            }
+        }
+
+        private bool Dragging;
+        private int xPos;
+        private int yPos;
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (Dragging && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos;
+                c.Left = e.X + c.Left - xPos;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging = true;
+                xPos = e.X;
+                yPos = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) { Dragging = false; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(hexValue.Text);
+        }
+    }
+}
